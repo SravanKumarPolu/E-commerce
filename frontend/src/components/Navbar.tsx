4fd26eed2f786f8306b8ee9e6d8a40e706061283
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2024-09-17 23:58:59 +0530

    few things added

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>NAVBAR of BuyMe</h1>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/pages/Prouct.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Prouct = () => {
+  return (
+    <div>Prouct</div>
+  )
+}
+
+export default Prouct
